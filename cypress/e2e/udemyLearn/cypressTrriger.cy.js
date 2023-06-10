d2fe588d1294d17bca2b221e9d142cd50954acad
Enessehirli <sehirlienes320@gmail.com>
2023-06-11 00:31:10 +0300

    XHR gizleme ve Mouseover kulanimi

---
+++ cypress/e2e/udemyLearn/cypressTrriger.cy.js
@@ -0,0 +1,7 @@
+describe('Trigger Kullanimi', () => {
+    it('Mouseover Kullanimi', () => {
+        cy.visit('https://www.amazon.com.tr/')
+        cy.get('span#nav-link-accountList-nav-line-1').trigger('mouseover')
+        cy.get("div#nav-al-your-account a:nth-child(2) > span").click()
+    });
+});
